95fb1a867dc3df1991a1cc2990ae62bfcdeae145
scharron <samuel.charron@gmail.com>
2014-07-29 12:13:08 +0200

    Update fuzz.py

--- fuzzywuzzy/fuzz.py
+++ fuzzywuzzy/fuzz.py
@@ -26,10 +26,10 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 
-try:
-    from .StringMatcher import StringMatcher as SequenceMatcher
-except:
-    from difflib import SequenceMatcher
+#try:
+#    from .StringMatcher import StringMatcher as SequenceMatcher
+#except:
+from difflib import SequenceMatcher
 
 from . import utils
 
